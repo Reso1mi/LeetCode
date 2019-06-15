f5327e179b8371d7840f15cb470fdbd41d3cc044
lgw <privateli@qq.com>
2019-06-15 12:28:15 +0800

    6.15

--- arrays/FindKthLargest215.java
+++ arrays/FindKthLargest215.java
@@ -1,28 +1,46 @@
 import java.util.PriorityQueue;
 public class FindKthLargest215{
 	public static void main(String[] args) {
-		int []nums={3,2,3,1,2,4,5,5,6};
-		//构建了大根堆
-		for (int i=0;i<nums.length;i++){
-			heapInsert(nums,i);
-		}
+		//int []nums={3,2,1,5,6,4};
+		
+		int []nums={3,2,3,1,2,4,5,5,6,23,-123,1323,41223,232,1,-213,2};
+		//NlogN 构建大根堆
+		/*for (int i=0;i<nums2.length;i++){heapInsert(nums2,i);}
+		printArray(nums2);
+		*/
+		heapSort(nums);
 		printArray(nums);
+		System.out.println(findKthLargest(nums,2));
+	}
 
-		System.out.println(findKthLargest(nums,10));
+
+	public static void heapSort(int []nums){
+		int last=nums.length-1;
+		//N构建大根堆
+		for (int i=nums.length/2-1 ;i>=0;i--){heapIfy(nums,i,last);}
+		//printArray(nums);
+		while(last>=1){
+			swap(nums,0,last--);
+			heapIfy(nums,0,last);
+		}
 	}
 
 	public static int findKthLargest(int[] nums, int k) {
 		//构建了大根堆
-		for (int i=0;i<nums.length;i++){
+		/*for (int i=0;i<nums.length;i++){
 			heapInsert(nums,i);
+		}*/
+		int last=nums.length-1;
+		for (int i=nums.length/2-1;i>=0;i--) {
+			heapIfy(nums,i,last);
 		}
-		int size=nums.length-1;
 		for (int i=0;i<k-1;i++) {
-			swap(nums,0,size);
-			heapSwim(nums,0,--size);
+			swap(nums,0,last);
+			heapIfy(nums,0,--last);
 		}
 		return nums[0];
 	}
+
 
 	public static void heapInsert(int[] nums,int i){
 		while(nums[i]>nums[(i-1)/2]){
@@ -32,14 +50,13 @@ public class FindKthLargest215{
 	}
 
     //i 变小 下沉
-	public static void heapSwim(int[] nums,int i,int size){
+	public static void heapIfy(int[] nums,int i,int last){
     	//判断有没有子节点（左孩子）
 		int left=i*2+1;
-		while(left<size){
+		while(left<=last){
 			int right=left+1;
     		//左右节点最大值
-    		//int larger=right>size||(right<size &&nums[right]<nums[left])?left:right;
-			int larger=left+1<size && nums[left]<nums[left+1] ?left+1:left;
+    		int larger=right<=last && nums[right] > nums[left]?right:left;
 			if(nums[larger]>nums[i]){
 				swap(nums,larger,i);
 				i=larger;
@@ -49,7 +66,7 @@ public class FindKthLargest215{
 			}
 		}
 	}
-
+	
 	public static  void  swap(int []nums,int a,int b){
 		int temp=nums[a];
 		nums[a]=nums[b];
@@ -65,5 +82,4 @@ public class FindKthLargest215{
 		}
 		System.out.println();
 	}
-
 }

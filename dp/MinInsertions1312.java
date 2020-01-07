3329d0a71d635eca5e8fcdd9a9c6061bbf3f03b1
lgw <privateli@qq.com>
2020-01-07 19:55:06 +0800

    talk is cheap

--- dp/MinInsertions1312.java
+++ dp/MinInsertions1312.java
@@ -4,6 +4,21 @@ public class MinInsertions1312{
     }
 
     public int minInsertions(String s) {
-        
+        if (s==null || s.length()<=0) {
+            return 0;
+        }
+        int n=s.length();
+        int[][] dp=new int[n][n];
+        for (int i=n-1;i>=0;i--) {
+            for (int j=i+1;j<n;j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j]=dp[i+1][j-1];   
+                }else{
+                    //不相等就可以在中间插入一个字符 +1
+                    dp[i][j]=1+Math.min(dp[i+1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][n-1];
     }
 }

8661d89b511724f11f7075513f64a03a28c67358
imlgw <privateli@qq.com>
2020-01-23 22:51:26 +0800

    talk is cheap

--- arrays/FindUnsortedSubarray581.java
+++ arrays/FindUnsortedSubarray581.java
@@ -3,6 +3,7 @@ public class FindUnsortedSubarray581{
 
     }
 
+    //O(1)空间的解法
     public int findUnsortedSubarray(int[] nums) {
         if (nums[0]>nums[nums.length-1]) {
             return nums.length;
@@ -10,13 +11,13 @@ public class FindUnsortedSubarray581{
         int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
         for (int i=1;i<nums.length;i++) {
             if (nums[i]<nums[i-1]) {
-                min=Math.min(min,nums[i]);
+                min=Math.min(min,nums[i]); //无序序列中的最小值
             }
         }
 
         for (int i=nums.length-1;i>0;i--) {
             if (nums[i]<nums[i-1]) {
-                max=Math.max(max,nums[i-1]);
+                max=Math.max(max,nums[i-1]); //无序序列中的最大值
             }
         }
         int left=0,right=nums.length-1;
@@ -24,17 +25,38 @@ public class FindUnsortedSubarray581{
             if (nums[left]>min) {
                 break;
             }
-            left++;
+            left++; //左边界正确位置
         }
         while(right>=0){
             if (nums[right]<max) {
                 break;
             }
-            right--;
+            right--; //右边界正确位置
         }
         return right<left?0:right-left+1;
     }
 
+    //单调栈的解法,很优秀
+    public int findUnsortedSubarray(int[] nums) {
+        Stack<Integer> stack=new Stack<>();
+        int left=Integer.MAX_VALUE,right=Integer.MIN_VALUE;
+        for (int i=0;i<nums.length;i++) {
+            while(!stack.isEmpty() && nums[stack.peek()]>nums[i]){
+                left=Math.min(stack.pop(),left); //左边界正确位置
+            }
+            stack.push(i);
+        }
+        stack.clear();
+        for (int i=nums.length-1;i>=0;i--) {
+            while(!stack.isEmpty() && nums[stack.peek()]<nums[i]){
+                right=Math.max(stack.pop(),right); //右边界正确位置
+            }
+            stack.push(i);
+        }
+        return left>right?0:right-left+1;
+    }
+
+    //SB做法WRONG ANSWER
     public int findUnsortedSubarray(int[] nums) {
         int i=0,j=nums.length-1;
         if (nums[i]>nums[j]) {

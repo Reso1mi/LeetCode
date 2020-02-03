28d0cee69a7262bae397814e02de03cf91ce2052
imlgw <privateli@qq.com>
2020-02-03 21:31:15 +0800

    talk is cheap

--- arrays/HIndex274.java
+++ arrays/HIndex274.java
@@ -4,21 +4,19 @@ public class HIndex274{
     }
 
     //3 0 6 1 5
-    //0 1 3 5 6
+    //6 5 3 2 1
     //
     //[12,2,12,13,22,231,45,789]
     //[2,12,12,13,22,45,231,789]
     public int hIndex(int[] citations) {
+        int len=citations.length;
         Arrays.sort(citations);
-        int min=0;
         int count=0;
-        for (int i=citations.length-1;i>=0;i--) {
-            count++;
-            min=citations[i];
-            if (min<=count-1) {
-                return count;
+        for (int i=len-1;i>=0;i--) {
+            if (citations[i]<=len-(i+1)) {
+                return len-(i+1);
             }
         }
-        return count;
+        return len;
     }
 }
